e1fc282b18ec05fdf18cc0a2116233552c4e63bb
unknown <malufreirejau@yahoo.com.br>
2022-05-03 15:55:02 -0300

    adding restart and finishing project

--- js/script.js
+++ js/script.js
@@ -431,8 +431,16 @@ function start() { // Inicio da função start()
         $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
     } // Fim da função gameOver();
 
-
-
 } // Fim da função start
+
+//Reinicia o Jogo
+
+function reiniciaJogo() {
+    somGameover.pause();
+    $("#fim").remove();
+    start();
+
+} //Fim da função reiniciaJogo
+
 
 
