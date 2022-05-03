04dcd97d40eafab2e6ace58213962dae9698e819
unknown <malufreirejau@yahoo.com.br>
2022-05-03 12:31:32 -0300

    adding readme and key moves

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jogo de Naves - Resgate - Bootcamp Órbi Dio :helicopter: 
+
+### Utilize a tecla W para movimentar o helicóptero para cima, a tecla S para movimentar o helicóptero para baixo e a tecla D para atirar.
+
+https://jogoresgateorbidiobymalu.netlify.app/
--- js/script.js
+++ js/script.js
@@ -11,6 +11,25 @@ function start() { // Inicio da função start()
 
     var jogo = {}
 
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla
+
+    $(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+
     //Game Loop
 
     jogo.timer = setInterval(loop,30);
@@ -18,6 +37,7 @@ function start() { // Inicio da função start()
     function loop() {
 
         movefundo();
+        movejogador();
 
     } // Fim da função loop()
 
@@ -29,6 +49,28 @@ function start() { // Inicio da função start()
         $("#fundoGame").css("background-position",esquerda-1);
 
     } // fim da função movefundo()
+
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo-10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo+10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama função Disparo
+        }
+
+    } // fim da função movejogador()
+
 
 } // Fim da função start
 
